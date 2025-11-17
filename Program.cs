6d659a3147ea63d7de7c659ea7813cc3748e4d31
Andrey Sukhanov <sukhanov.licey@gmail.com>
2025-11-17 12:50:57 +0300

    Первые изменения в репозитории

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using sandbox_csharp.physicParams;
+
+LengthParam length = new(50);
+SpeedParam speed1 = new(25);
+SpeedParam speed2 = new(10);
+LengthParam length2 = length / speed1 * speed2;
+Console.WriteLine(length2.Value);
---
+++ physicParams/PhysicParams.cs
@@ -0,0 +1,43 @@
+﻿namespace sandbox_csharp.physicParams;
+
+class LengthParam(double value)
+{
+    public double Value { get; private set; } = value;
+
+
+    public static TimeParam operator /(LengthParam a, SpeedParam b)
+    {
+        return new TimeParam(a.Value / b.Value);
+    }
+
+    public static SpeedParam operator /(LengthParam a, TimeParam b)
+    {
+        return new SpeedParam(a.Value / b.Value);
+    }
+}
+
+class SpeedParam(double value)
+{
+    public double Value { get; private set; } = value;
+}
+
+class TimeParam(double value)
+{
+    public double Value { get; private set; } = value;
+
+    public static LengthParam operator *(TimeParam a, SpeedParam b)
+    {
+        return new LengthParam(a.Value * b.Value);
+    }
+}
+
+enum PhysicType
+{
+    LENGTH,
+    SPEED,
+    ACCELERATION,
+    TIME
+}
+
+
+
